6cb6082b042e9fe7df72f5a259b4b8f56eb645a3
Richard Olaniyan <71474562+RichardOlaniyan@users.noreply.github.com>
2022-02-12 21:14:12 -0500

    back at it

---
+++ Project_One_FunFacts_React/index.js
@@ -0,0 +1,26 @@
+/* const Nav = 
+        <nav>
+            <a href=""><h1>Website</h1></a>
+            <ul>
+                <li><a href="">Pricing</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+
+ReactDOM.render(Nav, document.getElementById("root"))
+*/
+
+const comoros_fun_facts = 
+    <div>
+        <img src="./comoros_flag.png" width="150"></img>
+        <h1>Fun Facts about Comoros</h1>
+        <ul>
+            <li>Comoros is second only to Madagascar when it comes to vanilla production making it the second-largest vanilla producer in the entire world.</li>
+            <li>The entire country of Comoros has a police force of five hundred (500) officers. There is also a small standing army.</li>
+            <li>The islands of Comoros have three main exports. These exports are cloves, vanilla, and perfume essence.</li>
+            <li>The first inhabitants in Comoros is believed to have been Melanesian and Polynesian settlers.</li>
+        </ul>
+    </div>
+
+    ReactDOM.render(comoros_fun_facts, document.getElementById("root"))
