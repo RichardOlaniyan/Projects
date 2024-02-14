2d58ebc3b562ac5013d672201cda438bd5eb4af1
Richard <71474562+RichardOlaniyan@users.noreply.github.com>
2024-02-14 16:51:31 -0500

    Found a bug on the Prime Number program and fixed it.

--- Prime Number/main.py
+++ Prime Number/main.py
@@ -1,13 +1,22 @@
 # lil program to determine if a number entered by the user is Prime Number
 
+
 user_no = int(input("Enter number you want to check: "))
 
 
 def prime_checker(number=user_no):
-    if number % number == 0 and number % 1 == 0:
-        print(f"{number} is a prime number!")
-    else:
+    if number < 2:
         print(f"{number} is not a prime number")
+    else:
+        is_prime = True
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(f"{number} is a prime number!")
+        else:
+            print(f"{number} is not a prime number")
 
 
-print(prime_checker())
+prime_checker()
