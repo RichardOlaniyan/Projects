037c957495c278e489676a4f7e61481c97f5b70b
Richard <71474562+RichardOlaniyan@users.noreply.github.com>
2024-02-14 17:53:59 -0500

    Working on experience

--- Prime Number/main.py
+++ Prime Number/main.py
@@ -5,10 +5,10 @@ user_no = int(input("Enter number you want to check: "))
 
 
 def prime_checker(number=user_no):
+    is_prime = True
     if number < 2:
         print(f"{number} is not a prime number")
     else:
-        is_prime = True
         for i in range(2, int(number ** 0.5) + 1):
             if number % i == 0:
                 is_prime = False
@@ -17,6 +17,20 @@ def prime_checker(number=user_no):
             print(f"{number} is a prime number!")
         else:
             print(f"{number} is not a prime number")
+    return is_prime
 
 
-prime_checker()
+to_continue = True
+while to_continue:
+    command = prime_checker(user_no)
+    if command is False:
+        user_command_first = input('Try another number: \n')
+        if user_command_first == "retry":
+            to_continue = True
+    elif command is True:
+        user_command_second = input('Type Yes to continue or about to end \n')
+        if user_command_second == "abort":
+            to_continue = False
+else:
+    print("Thank you using the Prime Checker")
+
